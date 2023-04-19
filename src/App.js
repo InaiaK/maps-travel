3622300b7ad909845cbcc2e44e8bf1d44f046641
InaiaKenia <inaiakenia15@gmail.com>
2023-04-19 10:15:13 -0600

    created functional component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+// functional component 
+const App = () => {
+    return(
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
